f15dea1747c2e3208107aa8cd7add26d2e4c1200
Lucas Adelino <lcsadelino@gmail.com>
2025-07-18 12:46:05 -0400

    remove commented out colors

--- lua/conifer/palette.lua
+++ lua/conifer/palette.lua
@@ -1,6 +1,7 @@
 -- stylua: ignore
 
 local colors = {
+
   -- Code colors
   snow   = "#E1E1E1", -- white
   frost  = "#D6D6D6", -- fg
@@ -20,54 +21,28 @@ local colors = {
   brook  = "#BAD7FF", -- blue 
   fog    = "#576f82", -- gray_blue
   stone  = "#BDBBAF", -- pale_yellow
-  bark   = "#B49273", -- red
-  --
+  bark   = "#B49273", -- red 
+
   none = "NONE",
   light_gray_green = "#C3D3D2",
   cyan = "#60b197",
   green = "#85B884", -- leaf
-  --
+
   -- UI Colors
   azure = "#6196C2",
-  -- blue = "#3E61C1",
-  -- blue = "#2448AE",
-  -- blue = "#1D3C90",
-  -- blue = "#25459D",
-  -- blue = "#3961D0",
-  -- blue = "#3664E2",
   blue = "#416EEC",
   yellow  = "#FFE591",
-  -- dark_yellow = "#3e3f19",
-  -- dark_yellow = "#41421c",
-  -- dark_yellow = "#43441f",
-  -- dark_yellow = "#424420",
-  -- dark_yellow = "#474925",
-  -- dark_yellow = "#4f522d",
-  -- dark_yellow = "#555823",
   dark_yellow = "#555733",
-  -- dark_yellow = "#806801",
-  -- dark_yellow = "#786a3b",
-  -- dark_yellow = "#847546",
-  -- dark_yellow = "#cab87f",
-  -- dark_yellow = "#ceb461",
-  -- dark_yellow = "#c5b584",
-  -- dark_yellow = "#a59666",
-  -- dark_yellow = "#938544",
-  -- dark_yellow = "#806727",
-  -- dark_yellow = "#8c7334",
   red = "#D7867D",
-  -- dark_red = "#6d2722",
-  -- dark_red = "#692a2b",
-  -- dark_red = "#924841",
   dark_red = "#924843",
   dark_blue = "#133083",
   orange = "#F6AD6C",
   purple = "#AA749F",
   dark_green = "#164639",
-  medium_green = "#52a28a", 
+  medium_green = "#52a28a",
 
   bright_orange = "#FFA557",
-  --
+
   magenta = "#AA759F",
   cursor_fg = "#151515",
   cursor_bg = "#984936",
@@ -75,15 +50,8 @@ local colors = {
   sign_change = "#DBBB7F",
   sign_delete = "#D7867D",
   error = "#D7867D",
-  -- warning = "#F4BF75",
   warning = "#FFE591",
   info = "#ab8550",
-  -- hint = "#576f82", 
-  -- hint = "#6196C2", 
-  -- hint = "#48a298", 
-  -- hint = "#52a28a", 
-  -- hint = "#076751", 
-  -- hint = "#336253", 
   hint = "#164639", -- TODO: change
   todo = "#578266", -- TODO: change
   accent_lighter_blue = "#1e222a",
