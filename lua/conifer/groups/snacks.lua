d9cb7f308248c9a918d280635bf5ef571cb95a8f
Lucas Adelino <lcsadelino@gmail.com>
2025-12-03 20:42:41 -0500

    style(snacks): remove leftover comments

--- lua/conifer/groups/snacks.lua
+++ lua/conifer/groups/snacks.lua
@@ -3,13 +3,7 @@ local M = {}
 ---@param palette Colors
 function M.highlight(palette)
   return {
-    -- SnacksDashboardDesc = { fg = palette.green },
     SnacksDashboardFooter = { fg = palette.fg4 },
-    -- SnacksDashboardIcon = { fg = palette.fg },
-    -- SnacksDashboardSpecial = { fg = palette.fg },
-    -- SnacksPickerPrompt = { fg = palette.green },
-    -- SnacksPickerSpecial = { fg = palette.cyan },
-    -- SnacksPickerTitle = { fg = palette.green },
     SnacksDashboardKey = { fg = palette.leaf },
     SnacksIndent = { fg = palette.bg4 },
     SnacksIndentScope = { fg = palette.fg3 },
