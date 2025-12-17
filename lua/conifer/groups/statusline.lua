ffc75024998e2e4fa00c74dd6e80b2cd0f26b0f7
Lucas Adelino <lcsadelino@gmail.com>
2025-12-17 16:11:06 -0500

    feat(statusline): improve builtin statusline colors

--- lua/conifer/groups/statusline.lua
+++ lua/conifer/groups/statusline.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 ---@param palette Colors
-function M.highlight(palette, opts)
+function M.highlight(palette, _)
   return {
-    StatusLine = { fg = palette.bg4, bg = palette.bg0 },
+    StatusLine = { fg = palette.fg1, bg = palette.bg0 },
     StatusLineNC = { fg = palette.bg1, bg = palette.bg1 },
-    StatusLineSeparator = { fg = palette.bg0 },
+    StatusLineSeparator = { fg = palette.bg6 },
     StatusLineTerm = { fg = palette.bg0 },
     StatusLineTermNC = { fg = palette.bg0 },
   }
