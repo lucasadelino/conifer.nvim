8df5981eb2a5bb563ac9de7985ce19b9b4dcf17d
Lucas Adelino <lcsadelino@gmail.com>
2026-01-02 10:56:14 -0500

    feat(mini): add mini.clue hls

--- lua/conifer/groups/mini.lua
+++ lua/conifer/groups/mini.lua
@@ -3,6 +3,10 @@ local M = {}
 ---@param palette Colors
 function M.highlight(palette)
   return {
+    MiniClueDescGroup = { fg = palette.leaf },
+    MiniClueDescSingle = { fg = palette.fg4 },
+    MiniClueNextKey = { fg = palette.fg3 },
+    MiniClueSeparator = { fg = palette.bg4 },
     MiniHipatternsFixme = { fg = palette.bg0, bg = palette.dark_red, bold = true },
     -- TODO: Change this color's name to `medium orange` or smth
     MiniHipatternsHack = { fg = palette.bg0, bg = palette.info, bold = true },
