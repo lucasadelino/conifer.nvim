7bcb1802a2a5b25ab1a6a05fd68065997ba377d3
Lucas Adelino <lcsadelino@gmail.com>
2026-01-18 11:47:07 -0500

    feat(mini): add mini.diff highlights

--- lua/conifer/groups/mini.lua
+++ lua/conifer/groups/mini.lua
@@ -7,6 +7,10 @@ function M.highlight(palette)
     MiniClueDescSingle = { fg = palette.fg4 },
     MiniClueNextKey = { fg = palette.fg3 },
     MiniClueSeparator = { fg = palette.bg4 },
+    MiniDiffSignAdd = { fg = palette.sign_add },
+    MiniDiffSignChange = { fg = palette.sign_change },
+    MiniDiffSignDelete = { fg = palette.sign_delete },
+    GitSignsCurrentLineBlame = { fg = palette.bg4 },
     MiniHipatternsFixme = { fg = palette.bg0, bg = palette.dark_red, bold = true },
     -- TODO: Change this color's name to `medium orange` or smth
     MiniHipatternsHack = { fg = palette.bg0, bg = palette.info, bold = true },
