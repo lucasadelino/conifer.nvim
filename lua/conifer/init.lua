6891d42a45d5b161af164c18cc13b9e15d38f105
Lucas Adelino <lcsadelino@gmail.com>
2026-01-26 16:37:29 -0500

    docs(init): explain handling of background color mismatch

--- lua/conifer/init.lua
+++ lua/conifer/init.lua
@@ -10,13 +10,21 @@ function M.load(opts)
   local background = vim.o.background
   local style_bg = opts.variant == "solar" and "light" or "dark"
 
+  -- Check for a mismatch between `vim.o.background` and the expected bg color
+  -- for the current variant
   if background ~= style_bg then
     if
       (vim.g.colors_name == "conifer-solar" and opts.variant == "solar")
       or (vim.g.colors_name == "conifer" and opts.variant == "lunar")
     then
+      -- In this case, the user has likely changed `vim.o.background` at runtime,
+      -- so we change the variant to follow it.
       opts.variant = background == "light" and "solar" or "lunar"
     else
+      -- This happens if, for instance, the user has `vim.o.background` set to
+      -- `dark` (which is the Neovim default), but has specified `solar` as the
+      -- Conifer variant. In this case, we change the *background* to follow the
+      -- variant.
       vim.o.background = style_bg
     end
   end
